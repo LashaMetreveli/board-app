13a5c33965ea57caf46c53df3d4038b4d0c227ff
LashaMetreveli <lasha.metreveli.1@btu.edu.ge>
2021-01-17 00:25:05 +0400

    refactored CreateTask

---
+++ src/Components/Landing/CreateTask/Form/index.jsx
@@ -0,0 +1,96 @@
+import DropDown from "../../Util/DropDown";
+import Input from "../../Util/Input";
+import style from "./Form.module.scss";
+
+export default function Forms({
+  taskData,
+  setTaskData,
+  setErrors,
+  errors,
+  createTask,
+  toggleOverlay,
+}) {
+  return (
+    <div className={style.flex__container}>
+      <DropDown
+        taskData={taskData}
+        setTaskData={setTaskData}
+        clearError={() =>
+          setErrors({
+            ...errors,
+            ["board"]: null,
+          })
+        }
+        property="board"
+        label="Choose Board"
+        options={["Backlog", "To Do", "In Progress", "Done"]}
+      />
+      {errors["board"] && (
+        <span className={style.error}>{errors["board"]}</span>
+      )}
+      <Input
+        label="Task Title"
+        type="text"
+        placeholder={"title"}
+        property="title"
+        taskData={taskData}
+        setTaskData={setTaskData}
+        clearError={() =>
+          setErrors({
+            ...errors,
+            ["title"]: null,
+          })
+        }
+      />
+      {errors["title"] && (
+        <span className={style.error}>{errors["title"]}</span>
+      )}
+      <Input
+        label="Duo Date"
+        type="date"
+        property="duoDate"
+        taskData={taskData}
+        setTaskData={setTaskData}
+        clearError={() =>
+          setErrors({
+            ...errors,
+            ["duoDate"]: null,
+          })
+        }
+      />
+      {errors["duoDate"] && (
+        <span className={style.error}>{errors["duoDate"]}</span>
+      )}
+      <Input
+        label="Assigned To"
+        type="text"
+        placeholder={"assign to"}
+        property="assignedTo"
+        taskData={taskData}
+        setTaskData={setTaskData}
+        clearError={() =>
+          setErrors({
+            ...errors,
+            ["assignedTo"]: null,
+          })
+        }
+      />
+      {errors["assignedTo"] && (
+        <span className={style.error}>{errors["assignedTo"]}</span>
+      )}
+
+      <button
+        onClick={() => createTask()}
+        className={style.overlay__create__button}
+      >
+        Create
+      </button>
+      <button
+        onClick={() => toggleOverlay()}
+        className={style.overlay__close__button}
+      >
+        Close
+      </button>
+    </div>
+  );
+}
--- src/Components/Landing/CreateTask/index.jsx
+++ src/Components/Landing/CreateTask/index.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import Input from "../Util/Input";
 import uuid from "uuid/v4";
 import style from "./CreateTask.module.scss";
 import { columns as columnsData, items as itemsData } from "../Util/testData";
-import DropDown from "../Util/DropDown";
+import Forms from "./Form";
 
 export default function CreateTask({ setColumns, columns }) {
   const [overlay, toggleOverlay] = useState(false);
@@ -35,47 +34,44 @@ export default function CreateTask({ setColumns, columns }) {
   console.log(taskData);
 
   const createTask = () => {
-    // if (
-    //   taskData["duoDate"] === null ||
-    //   taskData["title"] === null ||
-    //   taskData["assignedTo"] === null ||
-    //   taskData["board"] === null ||
-    //   taskData["title"] === "" ||
-    //   taskData["assignedTo"] === ""
-    // ) {
-    //   setErrors({
-    //     ...errors,
-    //     ["title"]:
-    //       taskData["title"] === null || taskData["title"] === ""
-    //         ? "please enter correct title"
-    //         : null,
-    //     ["duoDate"]:
-    //       taskData["duoDate"] === null ? "please enter correct date" : null,
-    //     ["board"]: taskData["board"] === null ? "please choose Board" : null,
-    //     ["assignedTo"]:
-    //       taskData["assignedTo"] === null || taskData["assignedTo"] === ""
-    //         ? "please enter correct name"
-    //         : null,
-    //   });
-    // } else {
+    if (
+      taskData["duoDate"] === null ||
+      taskData["title"] === null ||
+      taskData["assignedTo"] === null ||
+      taskData["board"] === null ||
+      taskData["title"] === "" ||
+      taskData["assignedTo"] === ""
+    ) {
+      setErrors({
+        ...errors,
+        ["title"]:
+          taskData["title"] === null || taskData["title"] === ""
+            ? "please enter correct title"
+            : null,
+        ["duoDate"]:
+          taskData["duoDate"] === null ? "please enter correct date" : null,
+        ["board"]: taskData["board"] === null ? "please choose Board" : null,
+        ["assignedTo"]:
+          taskData["assignedTo"] === null || taskData["assignedTo"] === ""
+            ? "please enter correct name"
+            : null,
+      });
+    } else {
+      const columnName = columnsData[taskData.board].name;
+      const columnItems = columnsData[taskData.board].items;
+      columnItems.push({ id: uuid(), content: taskData.title });
 
-    // let columnItems = columnsData["Backglog"].items;
-    // columnItems.push({ id: uuid(), content: "my new task" });
+      setColumns({
+        ...columns,
+        [columnName]: {
+          name: columnName,
+          items: columnItems,
+        },
+      });
 
-    const columnName = columnsData[taskData.board].name;
-    const columnItems = columnsData[taskData.board].items;
-    columnItems.push({ id: uuid(), content: taskData.title });
-
-    setColumns({
-      ...columns,
-      [columnName]: {
-        name: columnName,
-        items: columnItems,
-      },
-    });
-
-    console.log(columnsData["Backglog"]);
-    // }
+      toggleOverlay();
+      clearTask();
+    }
   };
 
   return (
@@ -97,87 +93,14 @@ export default function CreateTask({ setColumns, columns }) {
           >
             X
           </button>
-          <div className={style.flex__container}>
-            <DropDown
-              taskData={taskData}
-              setTaskData={setTaskData}
-              clearError={() =>
-                setErrors({
-                  ...errors,
-                  ["board"]: null,
-                })
-              }
-              property="board"
-              label="Choose Board"
-              options={["BackLog", "To Do", "In Progress", "Done"]}
-            />
-            {errors["board"] && (
-              <span className={style.error}>{errors["board"]}</span>
-            )}
-            <Input
-              label="Task Title"
-              type="text"
-              placeholder={"title"}
-              property="title"
-              taskData={taskData}
-              setTaskData={setTaskData}
-              clearError={() =>
-                setErrors({
-                  ...errors,
-                  ["title"]: null,
-                })
-              }
-            />
-            {errors["title"] && (
-              <span className={style.error}>{errors["title"]}</span>
-            )}
-            <Input
-              label="Duo Date"
-              type="date"
-              property="duoDate"
-              taskData={taskData}
-              setTaskData={setTaskData}
-              clearError={() =>
-                setErrors({
-                  ...errors,
-                  ["duoDate"]: null,
-                })
-              }
-            />
-            {errors["duoDate"] && (
-              <span className={style.error}>{errors["duoDate"]}</span>
-            )}
-            <Input
-              label="Assigned To"
-              type="text"
-              placeholder={"assign to"}
-              property="assignedTo"
-              taskData={taskData}
-              setTaskData={setTaskData}
-              clearError={() =>
-                setErrors({
-                  ...errors,
-                  ["assignedTo"]: null,
-                })
-              }
-            />
-            {errors["assignedTo"] && (
-              <span className={style.error}>{errors["assignedTo"]}</span>
-            )}
-
-            <button
-              onClick={() => createTask()}
-              className={style.overlay__create__button}
-            >
-              Create
-            </button>
-            <button
-              onClick={() => toggleOverlay()}
-              className={style.overlay__close__button}
-            >
-              Close
-            </button>
-          </div>
+          <Forms
+            taskData={taskData}
+            setTaskData={setTaskData}
+            setErrors={setErrors}
+            errors={errors}
+            createTask={createTask}
+            toggleOverlay={toggleOverlay}
+          />
         </div>
       </div>
     </div>
